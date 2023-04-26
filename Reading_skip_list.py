2c6c99df4205726a0d5770a8b65aab83d7abc0cb
Alizain7866 <am07565@st.habib.edu.pk>
2023-04-27 01:06:37 +0500

    Update Reading_skip_list.py

--- Reading_skip_list.py
+++ Reading_skip_list.py
@@ -1,20 +1,91 @@
+## TODO find the complexity of the root node
+#find the complexity of the left most leaf node 
+#find the complexity of the right most leaf node
+
+
+
 import pandas as pd
 from Skip_list import SkipList
 import timeit
 import time 
-df = pd.read_csv('random_numbers_10k.csv', delimiter=' \n', header=None).values.tolist()
+data_reader_pandas = pd.read_csv('dataset_correct.csv', delimiter=' \n', header=None).values.tolist()
 # tree = AVLTree()
-#print(df)
-news = []
-
-for i in range (len(df)):
-    news.append(int(df[i][0]))
+#print(data_reader_pandas)
+Data_set_list = []
+output_list = []
 
 
-tre = SkipList()
-t1 = time.time()
-for i in news:
-    tre.insert(i)
-t2 = time.time() 
-final = t2-t1 
-print (final)
+for i in range (len(data_reader_pandas)):
+    Data_set_list.append(int(data_reader_pandas[i][0]))
+
+insert_num_1000 = 1000
+tre_1000 = SkipList()
+Initial_time_1000 = time.time()
+for i in range(insert_num_1000):
+    tre_1000.insert(Data_set_list[i])
+stopTime_1000 = time.time() 
+final_time_1000 = stopTime_1000-Initial_time_1000 
+print ("The time to insert_num_1000 is :",final_time_1000)
+
+output_list.append((insert_num_1000,final_time_1000))
+
+
+
+# 5000
+
+
+
+insert_num_5000 = 5000
+tre_5000 = SkipList()
+Initial_time_1000 = time.time()
+for i in range(insert_num_1000):
+    tre_5000.insert(Data_set_list[i])
+stopTime_5000 = time.time() 
+final_time_5000 = stopTime_1000-Initial_time_1000 
+print ("The time to insert_num_5000 is :",final_time_1000)
+
+output_list.append((insert_num_1000,final_time_1000))
+
+
+#10000
+
+insert_num_10000 = 10000
+tre_10000 = SkipList()
+Initial_time_10000 = time.time()
+for i in range(insert_num_1000):
+    tre_10000.insert(Data_set_list[i])
+stopTime_10000 = time.time() 
+final_time_10000 = stopTime_10000-Initial_time_1000 
+print ("The time to insert_num_10000 is :",final_time_1000)
+
+output_list.append((insert_num_10000,final_time_10000))
+
+
+#50k
+
+
+insert_num_50000 = 50000
+tre_50000 = SkipList()
+Initial_time_50000 = time.time()
+for i in range(insert_num_50000):
+    tre_50000.insert(Data_set_list[i])
+stopTime_50000 = time.time() 
+final_time_50000 = stopTime_50000-Initial_time_50000 
+print ("The time to insert_num_1000 is :",final_time_50000)
+
+output_list.append((insert_num_50000,final_time_50000))
+
+
+
+#100k
+
+insert_num_100k = 100000
+tre_100k = SkipList()
+Initial_time_100k = time.time()
+for i in range(insert_num_100k):
+    tre_100k.insert(Data_set_list[i])
+stopTime_100k = time.time() 
+final_time_100k = stopTime_100k-Initial_time_100k 
+print ("The time to insert_num_100k is :",final_time_100k)
+
+output_list.append((insert_num_100k,final_time_100k))
